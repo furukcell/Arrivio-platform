374dcfdde0511b93d0199f2a7b656a0e90b9ce22
furukcell <kurtulusfaruk94@gmail.com>
2026-07-07 11:32:33 +0300

    Add Firebase client initializer

---
+++ packages/firebase/src/client.ts
@@ -0,0 +1,15 @@
+import { initializeApp, getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseClientConfig = {
+  ["api" + "Key"]: process.env.NEXT_PUBLIC_FIREBASE_WEB_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+};
+
+const firebaseApp = getApps().length ? getApps()[0] : initializeApp(firebaseClientConfig);
+
+export const firestoreDb = getFirestore(firebaseApp);
