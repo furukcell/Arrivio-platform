228c299626476a66837c98d025efb318d01fcdad
furukcell <kurtulusfaruk94@gmail.com>
2026-07-07 11:00:35 +0300

    Add files via upload

---
+++ packages/firebase/src/collections.ts
@@ -0,0 +1,12 @@
+export const COLLECTIONS = {
+  users: "users",
+  transferRequests: "transferRequests",
+  carRentalRequests: "carRentalRequests",
+  hotelRequests: "hotelRequests",
+  ticketRequests: "ticketRequests",
+  providers: "providers",
+  qrSources: "qrSources",
+  qrEvents: "qrEvents",
+  commissions: "commissions",
+  adminLogs: "adminLogs"
+} as const;
