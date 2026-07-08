06a60b7289275be0b4e044ffd6d10d3ce0a30a57
furukcell <kurtulusfaruk94@gmail.com>
2026-07-08 17:26:39 +0300

    Load turquoise landing card overrides

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
 import "../src/styles/landingAtmosphere.css";
+import "../src/styles/landingTurquoiseCards.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
