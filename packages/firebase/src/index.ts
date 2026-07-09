10c9159b53900058dc2ce7fa0938de0aa5b7cfdc
furukcell <kurtulusfaruk94@gmail.com>
2026-07-09 13:26:33 +0300

    Export provider price helpers

--- packages/firebase/src/index.ts
+++ packages/firebase/src/index.ts
@@ -1,6 +1,7 @@
 export * from "./collections";
 export * from "./requests";
 export * from "./providers";
+export * from "./providerPrices";
 export * from "./auth";
 export * from "./users";
 export * from "./qr";
